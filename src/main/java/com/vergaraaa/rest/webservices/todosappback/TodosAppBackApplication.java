796aa4a357bee44133f245d4e1a04f07ba34935c
Edgar Vergara <edgarvm13@hotmail.com>
2023-11-27 19:09:37 +0100

    feat: init project

---
+++ src/main/java/com/vergaraaa/rest/webservices/todosappback/TodosAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.vergaraaa.rest.webservices.todosappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosAppBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vergaraaa/rest/webservices/todosappback/TodosAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vergaraaa.rest.webservices.todosappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
