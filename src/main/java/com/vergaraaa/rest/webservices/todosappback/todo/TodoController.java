0d51822e6525e44b0d38e2918894c77a5e2ed07d
Edgar Vergara <edgarvm13@hotmail.com>
2023-11-28 14:55:18 +0100

    feat: add post and put route to create and edit todo

--- src/main/java/com/vergaraaa/rest/webservices/todosappback/todo/TodoController.java
+++ src/main/java/com/vergaraaa/rest/webservices/todosappback/todo/TodoController.java
@@ -6,6 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,6 +28,22 @@ public class TodoController {
     @GetMapping("/users/{username}/todos/{id}")
     public Todo getUserTodo(@PathVariable String username, @PathVariable int id) {
         return todoService.findById(id);
+    }
+
+    @PostMapping("/users/{username}/todos/{id}")
+    public Todo createUserTodo(@PathVariable String username, @PathVariable int id,
+            @RequestBody Todo todo) {
+        Todo createdTodo = todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), false);
+
+        return createdTodo;
+    }
+
+    @PutMapping("/users/{username}/todos/{id}")
+    public Todo updateUserTodo(@PathVariable String username, @PathVariable int id,
+            @RequestBody Todo todo) {
+        todoService.updateTodo(todo);
+
+        return todo;
     }
 
     @DeleteMapping("/users/{username}/todos/{id}")
