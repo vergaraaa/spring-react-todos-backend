eb245a006e90713ba7b63041c5464f3e59e20b6c
Edgar Vergara <edgarvm13@hotmail.com>
2023-11-28 11:21:29 +0100

    feat: add get todo by id and delete todo with id

--- src/main/java/com/vergaraaa/rest/webservices/todosappback/todo/TodoController.java
+++ src/main/java/com/vergaraaa/rest/webservices/todosappback/todo/TodoController.java
@@ -2,6 +2,8 @@ package com.vergaraaa.rest.webservices.todosappback.todo;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +20,17 @@ public class TodoController {
     @GetMapping("/users/{username}/todos")
     public List<Todo> getUserTodos(@PathVariable String username) {
         return todoService.findByUsername(username);
+    }
+
+    @GetMapping("/users/{username}/todos/{id}")
+    public Todo getUserTodo(@PathVariable String username, @PathVariable int id) {
+        return todoService.findById(id);
+    }
+
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteUserTodo(@PathVariable String username, @PathVariable int id) {
+        todoService.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
